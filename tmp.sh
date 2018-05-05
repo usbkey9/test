251ce1f06b89994238ea2a1f87a1a0f63e8a8732
usb <usbkey9@gmail.com>
2018-05-05 19:57:08 +0200

    lol

--- tmp.sh
+++ tmp.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-gitup=$(./autoup);
+./autoup
+gitup="$?";
 
-if [[ $gitup -eq 1 ]]; then
+if [[ $gitup == 1 ]]; then
 	echo "Need to reload script";
 	echo "ArgsLOl : $@";
 	./$0 $@
-elif [[ $gitup -eq 2 ]]; then
+elif [[ $gitup == 2 ]]; then
 	echo "Error";
 else
 	echo "Continue";
